4c4e8283cc82ce72a9254340e2b5a9a75d5333cb
“joaomarcos” <Joaomarcos.spires@gmail.com>
2025-05-27 07:04:35 -0300

    Até que enfim eu consegui fazer o projeto maven funcionar caraio

---
+++ CursoJava_JPA_Hibernate/src/test/java/com/CursoJava_JPA_Hibernate/CursoJava_JPA_Hibernate/CursoJavaJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CursoJava_JPA_Hibernate.CursoJava_JPA_Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
