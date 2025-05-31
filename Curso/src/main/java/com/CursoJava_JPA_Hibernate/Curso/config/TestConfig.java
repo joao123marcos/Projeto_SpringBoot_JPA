55c6be8cca6cc4f0dbc055c9082070da3f234dd2
“joaomarcos” <Joaomarcos.spires@gmail.com>
2025-05-31 11:45:33 -0300

    Terminated implemens class OrderItem.

--- Curso/src/main/java/com/CursoJava_JPA_Hibernate/Curso/config/TestConfig.java
+++ Curso/src/main/java/com/CursoJava_JPA_Hibernate/Curso/config/TestConfig.java
@@ -9,11 +9,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.CursoJava_JPA_Hibernate.Curso.Repositories.CategoryRepository;
+import com.CursoJava_JPA_Hibernate.Curso.Repositories.OrderItemRepository;
 import com.CursoJava_JPA_Hibernate.Curso.Repositories.OrderRepository;
 import com.CursoJava_JPA_Hibernate.Curso.Repositories.ProductRepository;
 import com.CursoJava_JPA_Hibernate.Curso.Repositories.UserRepository;
 import com.CursoJava_JPA_Hibernate.Curso.entities.Category;
 import com.CursoJava_JPA_Hibernate.Curso.entities.Order;
+import com.CursoJava_JPA_Hibernate.Curso.entities.OrderItem;
 import com.CursoJava_JPA_Hibernate.Curso.entities.Product;
 import com.CursoJava_JPA_Hibernate.Curso.entities.User;
 import com.CursoJava_JPA_Hibernate.Curso.enums.OrderStatus;
@@ -22,83 +24,94 @@ import com.CursoJava_JPA_Hibernate.Curso.enums.OrderStatus;
 @Profile("test") // Vai executar apenas no perfil de teste no banco de dados
 public class TestConfig implements CommandLineRunner {
 
-    @Autowired // É a injeção de dependencia do framework
-    private UserRepository userRepository;
+        @Autowired // É a injeção de dependencia do framework
+        private UserRepository userRepository;
 
-    @Autowired
-    private OrderRepository orderRepository;
+        @Autowired
+        private OrderRepository orderRepository;
 
-    @Autowired
-    private CategoryRepository categoryRepository;
+        @Autowired
+        private CategoryRepository categoryRepository;
 
-    @Autowired
-    private ProductRepository productRepository;
+        @Autowired
+        private ProductRepository productRepository;
 
-    /*
-     * A CommandLineRunner vai executar tudo que tiver no método rum quando a
-     * aplicação for instanciada. É uma maneira se popular o banco quando a
-     * aplicação for
-     * instanciada.
-     */
-    @Override
-    public void run(String... args) throws Exception {
+        @Autowired
+        private OrderItemRepository orderItemRepository;
 
-        Category cat1 = new Category(null, "Electronics");
-        Category cat2 = new Category(null, "Books");
-        Category cat3 = new Category(null, "Computers");
+        /*
+         * A CommandLineRunner vai executar tudo que tiver no método rum quando a
+         * aplicação for instanciada. É uma maneira se popular o banco quando a
+         * aplicação for
+         * instanciada.
+         */
+        @Override
+        public void run(String... args) throws Exception {
 
-        Product p1 = new Product(null, "The Lord of the Rings", 
-          "Lorem ipsum dolor sit amet, consectetur.", 90.5, 
-          "");
-        
-        Product p2 = new Product(null, "Smart TV", 
-         "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, 
-         "");
+          Category cat1 = new Category(null, "Electronics");
+          Category cat2 = new Category(null, "Books");
+          Category cat3 = new Category(null, "Computers");
 
-        Product p3 = new Product(null, "Macbook Pro", 
-         "Nam eleifend maximus tortor, at mollis.", 1250.0, 
-         "");
+          Product p1 = new Product(null, "The Lord of the Rings",
+                            "Lorem ipsum dolor sit amet, consectetur.",
+                            90.5,"");
+          
+          Product p2 = new Product(null, "Smart TV",
+                            "Nulla eu imperdiet purus. Maecenas ante.",
+                            2190.0,"");
+           
+          Product p3 = new Product(null, "Macbook Pro",
+                            "Nam eleifend maximus tortor, at mollis.", 
+                            1250.0,"");
+          
+          Product p4 = new Product(null, "PC Gamer",
+                            "Donec aliquet odio ac rhoncus cursus.",
+                            1200.0,"");
+          
+          Product p5 = new Product(null, "Rails for Dummies",
+                            "Cras fringilla convallis sem vel faucibus.",
+                            100.99,"");
+           
+          productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+          categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+          
+          //Associando produto na categora
+          p1.getCategories().add(cat2);
+          p2.getCategories().add(cat1);
+          p2.getCategories().add(cat3);
+          p3.getCategories().add(cat3);
+          p4.getCategories().add(cat3);
+          p5.getCategories().add(cat2);
+           
+          productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
-        Product p4 = new Product(null, "PC Gamer", 
-         "Donec aliquet odio ac rhoncus cursus.", 1200.0, 
-         "");
-
-        Product p5 = new Product(null, "Rails for Dummies", 
-         "Cras fringilla convallis sem vel faucibus.", 100.99, 
-         "");        
-        
-        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-
-        p1.getCategories().add(cat2);
-        p2.getCategories().add(cat1);
-        p2.getCategories().add(cat3);
-        p3.getCategories().add(cat3);
-        p4.getCategories().add(cat3);
-        p5.getCategories().add(cat2);
-
-        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-
-        User u1 = new User(null, "Maria Brown", "maria@gmail.com",
-                "988888888", "123456");
-
-        User u2 = new User(null, "Alex Green", "alex@gmail.com",
-                "977777777", "123456");
-
-        Order o1 = new Order(null,
-                Instant.parse("2025-05-30T14:42:07Z"),
-                OrderStatus.valueOf("SHIPPED"), u1);
-
-        Order o2 = new Order(null,
-                Instant.parse("2025-05-30T15:42:10Z"),
-                OrderStatus.valueOf("WAITING_PAYMENT"), u2);
-
-        Order o3 = new Order(null,
-                Instant.parse("2025-05-30T16:21:22Z"),
-                OrderStatus.valueOf("DELIVERED"), u1);
-
-        userRepository.saveAll(Arrays.asList(u1, u2)); // salva no banco
-        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-    }
+          User u1 = new User(null, "Maria Brown", "maria@gmail.com",
+                            "988888888", "123456");
+          
+          User u2 = new User(null, "Alex Green", "alex@gmail.com",
+                            "977777777", "123456");
+          
+          Order o1 = new Order(null,
+                               Instant.parse("2025-05-30T14:42:07Z"),
+                               OrderStatus.valueOf("SHIPPED"), u1);
+          
+          Order o2 = new Order(null,
+                              Instant.parse("2025-05-30T15:42:10Z"),
+                              OrderStatus.valueOf("WAITING_PAYMENT"), u2);
+          
+          Order o3 = new Order(null,
+                               Instant.parse("2025-05-30T16:21:22Z"),
+                               OrderStatus.valueOf("DELIVERED"), u1);
+           
+           userRepository.saveAll(Arrays.asList(u1, u2)); // salva no banco
+           orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+           
+           OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+           OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+           OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+           OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+           
+           orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+        }
 
 }
--- Curso/src/main/java/com/CursoJava_JPA_Hibernate/Curso/entities/Order.java
+++ Curso/src/main/java/com/CursoJava_JPA_Hibernate/Curso/entities/Order.java
@@ -2,6 +2,8 @@ package com.CursoJava_JPA_Hibernate.Curso.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.CursoJava_JPA_Hibernate.Curso.enums.OrderStatus;
 
@@ -11,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +34,9 @@ public class Order implements Serializable{
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
+
     public Order() {
     }
 
@@ -40,6 +46,10 @@ public class Order implements Serializable{
         this.moment = moment;
         setOrderStatus(orderStatus);
         this.client = client;
+    }
+
+    public Set<OrderItem> getOrderItens(){
+        return items;
     }
 
     public Integer getId() {
